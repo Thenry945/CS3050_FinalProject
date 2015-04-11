1be53ebc4d807bfbd348d3a35ad17d5df0a595df
Mitchell Battles <mcbxx2@mail.missouri.edu>
2015-04-11 11:05:02 -0500

    Pushing to github

---
+++ src/BattlesHenrichsScully/Applicant.java
@@ -0,0 +1,39 @@
+package BattlesHenrichsScully;
+
+import java.util.ArrayList;
+
+public class Applicant{
+	
+	private String name;
+	private ArrayList<String> preferences = new ArrayList<String>();
+	/**
+	 * Creates a department with an array list of its preferences
+	 * @param name - the name of the department
+	 * @param vacancies - the number of vacancies in the department
+	 */
+	public Applicant(String name){
+		setName(name);
+	}
+	/*setter for name*/
+	private void setName(String name){
+		this.name = name;
+	}
+	/*Getter for name*/
+	public String getName(){
+		return name;
+	}
+	/**
+	 * adds a string to the preferences arraylist
+	 * @param input - the name of an applicant
+	 */
+	public void depAdd(String input){
+		this.preferences.add(input);
+	}
+	/**
+	 * takes in an arraylist index you want to get
+	 * @param x - the arrayList index you want to get
+	 */
+	public String depGet(int x){
+		return this.preferences.get(x);
+	}
+}
---
+++ src/BattlesHenrichsScully/Department.java
@@ -0,0 +1,48 @@
+package BattlesHenrichsScully;
+
+import java.util.ArrayList;
+
+public class Department{
+	
+	private String name;
+	private int vacancies;
+	private ArrayList<String> preferences = new ArrayList<String>();
+	/**
+	 * Creates a department with an array list of its preferences
+	 * @param name - the name of the department
+	 * @param vacancies - the number of vacancies in the department
+	 */
+	public Department(String name, int vacancies){
+		setName(name);
+		setVacancies(vacancies);
+	}
+	/*setters for name and vacancies*/
+	private void setName(String name){
+		this.name = name;
+	}
+	private void setVacancies(int vacancies){
+		this.vacancies = vacancies;
+	}
+	/*Getters for name and vacancies*/
+	public String getName(){
+		return name;
+	}
+	public int getVacancies(){
+		return vacancies;
+	}
+	
+	/**
+	 * adds a string to the preferences arraylist
+	 * @param input - the name of an applicant
+	 */
+	public void depAdd(String input){
+		this.preferences.add(input);
+	}
+	/**
+	 * takes in an arraylist index you want to get
+	 * @param x - the arrayList index you want to get
+	 */
+	public String depGet(int x){
+		return this.preferences.get(x);
+	}
+}
