4986c5b359e262d4f706e29930a0157109b2925a
tscully49 <thomas1374@yahoo.com>
2015-04-28 20:15:22 -0500

    added a for each loop through all departments

--- src/BattlesHenrichsScully/driver.java
+++ src/BattlesHenrichsScully/driver.java
@@ -11,7 +11,8 @@ public class Driver{
 		readInFile fileList = new readInFile("inputFiles/JobsApplicants.txt");
 		//ArrayList<String> fileReturn = fileList.readFile();
 		
-		try {
+		try 
+		{
 			ArrayList<String> fileReturn = fileList.readFile();
 			parse parseFile = new parse(fileReturn);
 			//System.out.println(fileReturn.get(0));
@@ -19,11 +20,18 @@ public class Driver{
 			//System.out.println(parseFile.getAppList().get(0).getName());
 			//System.out.println(parseFile.getDepList().get(0).depGet(0));
 			//System.out.println(parseFile.getAppList().get(0).appGet(0));
-			} 
+			ArrayList<Department> all_deps = parseFile.getDepList();
+			ArrayList<Applicant> all_apps = parseFile.getAppList();
+			
+			for (int counter = 0; counter < all_deps.size(); counter++) {
+				System.out.println(all_deps.get(counter).getName());
+			}
+		} 
 		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			}
+		}
+		
 			
 	}
 }
